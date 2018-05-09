bf75af845890daa6598e0b30393cc853b11fe380
Андрей Панасенко <xambey@yandex.ru>
2018-05-10 02:29:56 +0300

    Create system directory

---
+++ Core/Messages/MessageDecoder.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Messages
+{
+    /// <summary>
+    /// MessageDecoder determinate type of messages and call needed handler
+    /// </summary>
+    public class MessageDecoder
+    {
+        public void DecodeMessage(string message)
+        {
+            
+        }
+    }
+}
